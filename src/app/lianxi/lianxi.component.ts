e7f18baeb341e328491f5e36fe73ad55a956cde2
wcnvidia <158677136@qq.com>
2017-04-08 20:25:25 +0800

    增加百度地图功能

--- src/app/lianxi/lianxi.component.ts
+++ src/app/lianxi/lianxi.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { BaiduMap, OfflineOptions, ControlAnchor, NavigationControlType } from 'angular2-baidu-map';
 
 @Component({
   selector: 'app-lianxi',
@@ -8,10 +8,49 @@ import { Component, OnInit } from '@angular/core';
 
 })
 export class LianxiComponent implements OnInit {
+  opts: any;
+  offlineOpts: OfflineOptions;
 
   constructor() { }
 
   ngOnInit() {
+    this.opts = {
+      center: {
+        longitude: 103.890724,
+        latitude: 30.492527
+      },
+      zoom: 17,
+      markers: [{
+        longitude: 103.890724,
+        latitude: 30.492527,
+        title: '成都艺术职业学院',
+        content: '花源镇白云大道115号',
+        enableDragging: true
+      }],
+      geolocationCtrl: {
+        anchor: ControlAnchor.BMAP_ANCHOR_BOTTOM_RIGHT
+      },
+      scaleCtrl: {
+        anchor: ControlAnchor.BMAP_ANCHOR_BOTTOM_LEFT
+      },
+      overviewCtrl: {
+        isOpen: true
+      },
+      navCtrl: {
+        type: NavigationControlType.BMAP_NAVIGATION_CONTROL_LARGE
+      }
+    }
+    this.offlineOpts = {
+      retryInterval: 5000,
+      txt: '网络连接较慢，请刷新'
+    };
+  }
+  loadMap(map: any) {
+    console.log('map instance here', map);
   }
 
+  clickMarker(marker: any) {
+    console.log('The clicked marker is', marker);
+  }
 }
+
