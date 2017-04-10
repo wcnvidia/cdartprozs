cb19a9e395def6ffb0de19b492f90ed22f7fa90b
wcnvidia <158677136@qq.com>
2017-04-10 14:52:05 +0800

    高德地图中增加了位置

--- src/app/lianxi/lianxi.component.ts
+++ src/app/lianxi/lianxi.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-
+declare var AMap: any;
 @Component({
   selector: 'app-lianxi',
   templateUrl: './lianxi.component.html',
@@ -13,30 +13,28 @@ export class LianxiComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-let map = new AMap.Map('gaodemap-container');
-        map.plugin('AMap.Geolocation', () => {
-             let geolocation = new AMap.Geolocation({
-                enableHighAccuracy: true,//是否使用高精度定位，默认:true
-                 timeout: 10000,          //超过10秒后停止定位，默认：无穷大
-                 maximumAge: 0,           //定位结果缓存0毫秒，默认：0
-                 convert: true,           //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
-                 showButton: true,        //显示定位按钮，默认：true
-                 buttonPosition: 'LB',    //定位按钮停靠位置，默认：'LB'，左下角
-                 buttonOffset: new AMap.Pixel(10, 20),//定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
-                 showMarker: true,        //定位成功后在定位到的位置显示点标记，默认：true
-                 showCircle: true,        //定位成功后用圆圈表示定位精度范围，默认：true
-                 panToLocation: true,     //定位成功后将定位到的位置作为地图中心点，默认：true
-                zoomToAccuracy: true     //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
-            });
-             map.addControl(geolocation);
-        });
-
-  
-
-
-
-
-}
+    let map = new AMap.Map('gaodemap-container', {
+      zoom: 15,
+      center: [103.885863, 30.487819],
+      mapStyle: 'fresh'
+    });
+    map.plugin(['AMap.OverView', 'AMap.Scale', 'AMap.ToolBar'], () => {
+      let toolbar = new AMap.ToolBar();
+      let overview = new AMap.OverView({ isOpen: true });
+      let scale = new AMap.Scale();
+      map.addControl(toolbar);
+      map.addControl(overview);
+      map.addControl(scale)
+    });
+    let marker = new AMap.Marker({
+      position: [103.885863, 30.487819]
+    });
+    marker.setMap(map);
+    let info = new AMap.InfoWindow({
+      content: "<h4>成都艺术职业学院</h4><p>新津县花源镇白云大道115号</p>"
+    })
+    info.open(map, marker.getPosition())
+  }
 
 }
 
