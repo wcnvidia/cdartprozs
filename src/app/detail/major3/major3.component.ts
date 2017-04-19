c668350db86a2b691efa85c6a6c76c55ee47c819
wcnvidia <158677136@qq.com>
2017-04-19 22:27:43 +0800

    制作好各个专业模块模版

---
+++ src/app/detail/major3/major3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-major2',
+  templateUrl: './major2.component.html',
+  styleUrls: ['./major2.component.css']
+})
+export class Major2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
