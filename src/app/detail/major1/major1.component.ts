b068cbaf7e774933352ea9307f8cd9e560b05933
wcnvidia <158677136@qq.com>
2017-04-22 23:22:30 +0800

    给major做了模版

--- src/app/detail/major1/major1.component.ts
+++ src/app/detail/major1/major1.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-major1',
   templateUrl: './major1.component.html',
-  styleUrls: ['./major1.component.css']
+  styleUrls: ['./major1.component.css'],
 })
 export class Major1Component implements OnInit {
 
--- src/app/detail/major1/major1.module.ts
+++ src/app/detail/major1/major1.module.ts
@@ -1,5 +1,4 @@
 import { Major1RoutingModule } from './major1.routes';
-
 import { Major1Component } from './major1.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
